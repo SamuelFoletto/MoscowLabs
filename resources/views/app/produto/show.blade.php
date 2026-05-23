67e14c25a3bcf7fb7de08442766311ceb7219b4c
Samuel Foletto <sfoletto@gmail.com>
2026-05-22 21:15:24 -0300

    Teste

--- resources/views/app/produto/show.blade.php
+++ resources/views/app/produto/show.blade.php
@@ -11,6 +11,7 @@
             <h2>{{$produto->nome}}</h2>
             <p class="produto__value">R$ {{number_format($produto->valor, 2, ',', '.')}}</p>
             <p>Ou 3x de <b>R${{number_format($produto->valor / 3, 2, ',', '.')}}</b> sem juros</p>
+            <p>Teste Teste Teste Teste</p>
         </div>
     </div>
 
